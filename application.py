d7bff672aba13a2789548c2c8edc69dba20ec97a
Daniel Hong <daniel@danielhongsair.bc.edu>
2022-01-24 17:41:34 -0500

    name

--- application.py
+++ application.py
@@ -7,6 +7,10 @@ application = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@application.route('/about')
+def about():
+    return render_template("about.html")
+
 @application.route('/contact')
 def contact():
     return render_template("contact.html")
